b2020665dfe411a6dc66d451f89980dec46fb093
piflynn <piflynn@me.com>
2017-02-19 15:07:33 -0500

    First big commit

---
+++ js/script.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+  var tempC;
+  var tempF;
+  var celsius;
+
+  $.ajax({
+        type: "GET",
+        url: 'http://api.wunderground.com/api/c50c16deb2f1c9af/conditions/q/VA/Norfolk.json'
+      }).done(function(json) {
+        
+
+        tempF = json.current_observation.temp_f + " &deg;F";
+        tempC = json.current_observation.temp_c + " &deg;C";
+        $("#data").html(tempF);
+        celsius = false;
+
+        $("#icon").attr("src",json.current_observation.icon_url);
+        $("#wu").attr("src",json.current_observation.image.url);
+      });
+  
+  $("#deg").click(function() {
+    if(!celsius) {
+      $("#data").toggle("slow",function() {
+        $("#data").html(tempC);
+        celsius = true;
+        $("#data").toggle("slow");
+        });
+    }
+    else {
+      $("#data").toggle("slow",function() {
+        $("#data").html(tempF);
+        celsius = false;
+        $("#data").toggle("slow");
+        });
+    }
+  });
+  
+});
